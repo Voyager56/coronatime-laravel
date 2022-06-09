5e2b42bf29abe8462f8edf44515e528e00514d31
Voyager56 <levan@redberry.ge>
2022-06-09 16:16:35 +0400

    refactor: group routes into auth and guest middleware

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/email/verify', [EmailController::class, 'verifyEmailView'])->middleware('auth')->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}', [EmailController::class, 'confirmEmail'])->middleware(['auth'])->name('verification.verify');
-Route::post('/email/verification-notification', [EmailController::class, 'emailSentView'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-Route::view('/', 'sessions.login')->middleware('guest')->name('login');
-Route::post('/login', [SessionController::class, 'loginUser'])->name('login.store');
-Route::post('/logout', [SessionController::class, 'logoutUser'])->name('logout');
-
-Route::view('/signup', 'sessions.register')->name('register');
-Route::post('/signup', [RegistrationController::class, 'registration'])->name('register');
-
-Route::get('/home', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
-Route::get('/countries', [DashboardController::class, 'show'])->middleware(['auth', 'verified'])->name('show');
-
-Route::view('/forgot-password', 'auth.reset')->middleware('guest')->name('password.request');
-Route::post('/forgot-password', [PasswordResetController::class, 'sendPasswordResetEmail'])->middleware('guest')->name('password.email');
-Route::get('/reset-password/{token}', [PasswordResetController::class, 'resetPassword'])->middleware('guest')->name('password.reset');
-Route::post('/reset-password', [PasswordResetController::class, 'saveNewPassword'])->middleware('guest')->name('password.update');
-
 Route::get('locale/{lang}', [LangController::class, 'index'])->name('lang');
+
+Route::middleware('auth')->group(function () {
+	Route::get('/email/verify', [EmailController::class, 'verifyEmailView'])->name('verification.notice');
+	Route::get('/email/verify/{id}/{hash}', [EmailController::class, 'confirmEmail'])->name('verification.verify');
+	Route::post('/email/verification-notification', [EmailController::class, 'emailSentView'])->middleware('throttle:6,1')->name('verification.send');
+	Route::get('/home', [DashboardController::class, 'index'])->middleware('verified')->name('home');
+	Route::get('/countries', [DashboardController::class, 'show'])->middleware('verified')->name('show');
+	Route::post('/logout', [SessionController::class, 'logoutUser'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+	Route::view('/', 'sessions.login')->name('login');
+	Route::post('/login', [SessionController::class, 'loginUser'])->name('login.store');
+
+	Route::view('/signup', 'sessions.register')->name('register');
+	Route::post('/signup', [RegistrationController::class, 'registration'])->name('register');
+
+	Route::view('/forgot-password', 'auth.reset')->name('password.request');
+	Route::post('/forgot-password', [PasswordResetController::class, 'sendPasswordResetEmail'])->name('password.email');
+	Route::get('/reset-password/{token}', [PasswordResetController::class, 'resetPassword'])->name('password.reset');
+	Route::post('/reset-password', [PasswordResetController::class, 'saveNewPassword'])->name('password.update');
+});
