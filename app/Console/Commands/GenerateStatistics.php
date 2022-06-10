827f81789de80847bd613b4761f2d4a41b508cf9
Voyager56 <levan@redberry.ge>
2022-06-10 15:13:17 +0400

    refactor: name command more intuitive name

--- app/Console/Commands/GenerateStatistics.php
+++ app/Console/Commands/GenerateStatistics.php
@@ -7,14 +7,14 @@ use App\Models\Country;
 use App\Models\Statistic;
 use Illuminate\Console\Command;
 
-class FetchDatabase extends Command
+class GenerateStatistics extends Command
 {
 	/**
 	 * The name and signature of the console command.
 	 *
 	 * @var string
 	 */
-	protected $signature = 'fetch:database';
+	protected $signature = 'generate:database';
 
 	/**
 	 * The console command description.
